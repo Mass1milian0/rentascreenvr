df13aea257a21fd572ac25c60652b5591af48637
SlyTheFloof <56106431+SlyTheFloof@users.noreply.github.com>
2024-06-21 09:13:12 -0400

    reverted contacts edit

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { createRenderer } from 'vue';
-
 definePageMeta({
   layout: "landing"
 });
@@ -29,17 +27,16 @@ if (route.query.screen) {
     <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-16">
       <div>
         <h2 class="font-medium text-2xl text-gray-800">
-          Contact The Rent A Screen VR Creator
+          Contact Rent A Screen VR Creator
         </h2>
         <p class="text-lg leading-relaxed text-slate-500 mt-3">
-          Have something to say?<br>Want to report a Screen?<br>
-          I'm here to help.<br>
-          Fill up the Form or Send Email.
+          Have something to say? I am here to help. Fill up the form or send
+          email.
         </p>
         <div class="mt-5">
           <div class="flex items-center mt-2 space-x-2 text-gray-600">
             <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker" />
-            <span>20-22 Wenlock Rd London N1 7GU, UK</span>
+            <span>20-22 Wenlock Road,London</span>
           </div>
           <div class="flex items-center mt-2 space-x-2 text-gray-600">
             <Icon class="text-gray-400 w-4 h-4" name="uil:envelope" /><a href="mailto:massimilianobiondi07@gmail.com"
